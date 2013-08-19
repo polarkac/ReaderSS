e87d213aa05fec20f5b93cd371b11f049bb77d2f
Lukas Pohlreich <pohlreichlukas@gmail.com>
2013-08-19 18:29:33 +0200

    Update to Django 1.5.2 and rewrite login form

--- ReaderSS/homepage/models.py
+++ ReaderSS/homepage/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django import forms
+from django.contrib.auth import authenticate, login
 
 class Feeds( models.Model ):
     name = models.CharField( max_length = 130 )
@@ -9,5 +10,31 @@ class Feeds( models.Model ):
         return self.name
 
 class AuthForm( forms.Form ):
+    request = None
+    user = None
     username = forms.CharField( max_length = 30 )
     password = forms.CharField( max_length = 30, widget = forms.PasswordInput() )
+
+    def __init__( self, argRequest = None, *args, **kwargs ):
+        self.request = argRequest;
+        super( AuthForm, self ).__init__( *args, **kwargs )
+
+    def clean( self ):
+        cleanedUsername = self.cleaned_data['username']
+        cleanedPassword = self.cleaned_data['password']
+
+        if cleanedUsername and cleanedPassword:
+            self.user = authenticate( username = cleanedUsername, password = cleanedPassword )
+            if self.user is None:
+                raise forms.ValidationError( 'Invalid username or password.', 
+                    code = 'invalid_login' )
+            else:
+                self.loginUser()
+
+        return self.cleaned_data
+
+    def loginUser( self ):
+        login( self.request, self.user )
+
+    def getUser( self ):
+        return self.user
--- ReaderSS/homepage/views.py
+++ ReaderSS/homepage/views.py
@@ -2,28 +2,24 @@ from django.shortcuts import render, redirect
 from homepage.models import Feeds, AuthForm
 import feedparser
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponseRedirect
+from django.core.exceptions import PermissionDenied
+from django import forms
 
 def index( request ):
-    feeds = Feeds.objects.all()
-    parsedFeeds = dict() 
-    for f in feeds:
-        parsedFeeds[ f.name ] = feedparser.parse( f.url )
-    context = { 'title': 'ReaderSS', 'feeds': parsedFeeds, 'user': request.user }
+    context = { 'title': 'ReaderSS' }
 
     if request.method == 'POST':
-        form = AuthForm( request.POST )
+        form = AuthForm( request, request.POST )
         if form.is_valid():
-            user = authenticate( username = form.cleaned_data['username'], password = form.cleaned_data['password'] )
-            if user is not None:
-                login( request, user )
-                return redirect( '/' )
+            if form.getUser() is not None:
+                redirect( '/' )
     else:
-        form = AuthForm()
+        form = AuthForm( request )
 
     context['form'] = form;
+    context['user'] = request.user
 
     return render( request, 'homepage/homepage.html', context )
 
